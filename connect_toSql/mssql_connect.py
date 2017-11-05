8bf3f963545c5cbdef35890a4ad3ae292ccba5cb
Yeshwanth Nagaraj <ynagaraj@aptean.com>
2017-11-05 14:17:24 +0530

    connect to mssql server

---
+++ connect_toSql/mssql_connect.py
@@ -0,0 +1,13 @@
+import pyodbc
+server = 'WS003LT2659PRD\MSSQL14'
+database = 'EnterprisePowerManagementDB'
+username = 'sa'
+password = 'abc-123'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+cursor.execute("select top 10 * from Action;")
+row = cursor.fetchone()
+while row:
+    print (row[0],row[1])
+    row = cursor.fetchone()
